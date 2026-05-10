67baacfcf547b5fa5a54812db2e9b4f1b58b037e
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-10 09:42:30 +0530

    init commit

---
+++ src/main/java/org/pos/retailpossystem/RetailPosSystemApplication.java
@@ -0,0 +1,13 @@
+package org.pos.retailpossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailPosSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailPosSystemApplication.class, args);
+    }
+
+}
