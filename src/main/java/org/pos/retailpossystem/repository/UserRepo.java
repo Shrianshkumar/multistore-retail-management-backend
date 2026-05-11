9f8a78a347e7e052637931464691896cdb00cf35
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-11 13:50:29 +0530

    created repository for User entity

---
+++ src/main/java/org/pos/retailpossystem/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package org.pos.retailpossystem.repository;
+
+import org.pos.retailpossystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
