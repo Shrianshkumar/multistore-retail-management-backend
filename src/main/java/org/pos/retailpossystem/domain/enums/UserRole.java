ff6e6cc24bdc51c32e9dbe541466cbc2280544cb
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-11 13:37:34 +0530

    created UserRole enum

---
+++ src/main/java/org/pos/retailpossystem/domain/enums/UserRole.java
@@ -0,0 +1,11 @@
+package org.pos.retailpossystem.domain.enums;
+
+public enum UserRole {
+    ROLE_ADMIN,
+    ROLE_STORE_ADMIN,
+    ROLE_STORE_MANAGER,
+    ROLE_BRANCH_MANAGER,
+    ROLE_BRANCH_ADMIN,
+    ROLE_BRANCH_CASHIER,
+    ROLE_CUSTOMER
+}
