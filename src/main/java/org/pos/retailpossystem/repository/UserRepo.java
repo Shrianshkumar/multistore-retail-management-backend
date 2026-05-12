f2f994c5b77d20697cc26024927a5448f8b79dda
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-12 14:19:09 +0530

    user repository interface

--- src/main/java/org/pos/retailpossystem/repository/UserRepo.java
+++ src/main/java/org/pos/retailpossystem/repository/UserRepo.java
@@ -1,10 +1,17 @@
 package org.pos.retailpossystem.repository;
 
+import org.pos.retailpossystem.domain.enums.UserRole;
 import org.pos.retailpossystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    Set<User> findByRole(UserRole role);
+    List<User> findByBranchId(Long branchId);
+    List<User>findByStoreId(Long storeId);
 }
