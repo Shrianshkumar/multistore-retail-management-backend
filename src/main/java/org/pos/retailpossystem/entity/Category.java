3cec5b5fae6e29f58980dba1c3510c71ff49ed95
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-14 16:04:35 +0530

    Category Entity Class

---
+++ src/main/java/org/pos/retailpossystem/entity/Category.java
@@ -0,0 +1,22 @@
+package org.pos.retailpossystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private Store store;
+}
