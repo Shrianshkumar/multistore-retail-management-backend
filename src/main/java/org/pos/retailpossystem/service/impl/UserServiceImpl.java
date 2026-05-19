fbbb034f90a5b634ddf942808a445879c6acfcfb
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-19 13:32:52 +0530

    fixed errors

--- src/main/java/org/pos/retailpossystem/service/impl/UserServiceImpl.java
+++ src/main/java/org/pos/retailpossystem/service/impl/UserServiceImpl.java
@@ -1,11 +1,10 @@
 package org.pos.retailpossystem.service.impl;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.pos.retailpossystem.configuration.JwtProvider;
 import org.pos.retailpossystem.domain.enums.UserRole;
 import org.pos.retailpossystem.entity.User;
-import org.pos.retailpossystem.exception.UserException;
+import org.pos.retailpossystem.exception.ResourceNotFoundException;
 import org.pos.retailpossystem.repository.UserRepo;
 import org.pos.retailpossystem.service.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,69 +20,63 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepo userRepo;
     private final JwtProvider jwtProvider;
-    /**
-     * @param email
-     * @return user
-     */
+
     @Override
-    public User getUserByEmail(String email) throws UserException {
-        User user=userRepo.findByEmail(email);
-        if(user==null){
-            throw new UserException("User not found with email: "+email);
-        }
-        return user;
+    public User getUserByEmail(String email) {
+
+        return userRepo.findByEmail(email);
     }
 
-    /**
-     * @param id
-     * @return
-     */
     @Override
-    public User getUserById(Long id) throws UserException {
-        return userRepo.findById(id).orElse(null);
+    public User getUserById(Long userId) {
+
+        return userRepo.findById(userId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found"));
     }
 
-    /**
-     * @param token
-     * @return
-     */
     @Override
-    public User getUserFromJwtToken(String token) throws UserException {
+    public User getUserFromJwtToken(String token) {
+
         String email = jwtProvider.getEmailFromJwtToken(token);
+
         User user = userRepo.findByEmail(email);
-        if(user==null) throw new UserException("user not exist with email "+email);
+
+        if (user == null) {
+            throw new ResourceNotFoundException(
+                    "User not found with email: " + email
+            );
+        }
+
         return user;
     }
 
-    /**
-     * @return user
-     */
     @Override
     public User getCurrentUser() {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user= userRepo.findByEmail(email);
-        if(user == null) {
-            throw new EntityNotFoundException("User not found");
+
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+
+        User user = userRepo.findByEmail(email);
+
+        if (user == null) {
+            throw new ResourceNotFoundException("User not found");
         }
+
         return user;
     }
 
-    /**
-     * @param role
-     * @return
-     * @throws UserException
-     */
     @Override
-    public Set<User> getUserByRole(UserRole role) throws UserException {
+    public Set<User> getUsersByRole(UserRole role) {
+
         return userRepo.findByRole(role);
     }
 
-    /**
-     * @return
-     * @throws UserException
-     */
     @Override
-    public List<User> getUsers() throws UserException {
+    public List<User> getUsers() {
+
         return userRepo.findAll();
     }
 }
