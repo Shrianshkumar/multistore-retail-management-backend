9a47f876110259a491acb25a060899c62bb1529d
Shrianshkumar <shreyaskr76@gmail.com>
2026-05-29 17:08:52 +0530

    mapper class of Branch

---
+++ src/main/java/org/pos/retailpossystem/mapper/BranchMapper.java
@@ -0,0 +1,43 @@
+package org.pos.retailpossystem.mapper;
+
+import org.pos.retailpossystem.entity.Branch;
+import org.pos.retailpossystem.entity.Store;
+import org.pos.retailpossystem.payload.dto.BranchDto;
+
+public class BranchMapper {
+    public static BranchDto toDto(Branch branch) {
+        return BranchDto.builder()
+                .id(branch.getId())
+                .name(branch.getName())
+                .address(branch.getAddress())
+                .phone(branch.getPhone())
+                .email(branch.getEmail())
+                .closeTime(branch.getCloseTime())
+                .openTime(branch.getOpenTime())
+                .workingDays(branch.getWorkingDays())
+                .storeId(branch.getStore() != null ? branch.getStore().getId() : null)
+                .store(StoreMapper.mapToDto(branch.getStore()))
+                .createdAt(branch.getCreatedAt())
+                .updatedAt(branch.getUpdatedAt())
+                .manager(branch.getManager()!=null?
+                        branch.getManager().getFullName():null)
+                .build();
+    }
+
+    public static Branch mapToEntity(BranchDto dto, Store store) {
+        return Branch.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .address(dto.getAddress())
+                .store(store)
+                .email(dto.getEmail())
+                .phone(dto.getPhone())
+                .closeTime(dto.getCloseTime())
+                .openTime(dto.getOpenTime())
+                .workingDays(dto.getWorkingDays())
+                .createdAt(dto.getCreatedAt())
+                .updatedAt(dto.getUpdatedAt())
+
+                .build();
+    }
+}
